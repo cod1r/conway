442e07f4608e82d3654ed25ec8337de779c2c1eb
cod1r <jasonho353@gmail.com>
2023-10-27 02:59:02 -0500

    got the colors right. all zeroes is black which is what I want, all ones is white which means dead

--- src/conway.ts
+++ src/conway.ts
@@ -7,20 +7,26 @@ const BUFFER_WIDTH = 100;
 const NODE_WIDTH = CANVAS_WIDTH / BUFFER_WIDTH;
 const NODE_HEIGHT = CANVAS_HEIGHT / BUFFER_HEIGHT;
 let bytes = new Uint8ClampedArray(BUFFER_HEIGHT * BUFFER_WIDTH * 3 /* times 3 because of RGB */);
+const DEAD = 1;
+const ALIVE = 0;
+for (let i = 0; i < BUFFER_HEIGHT * BUFFER_WIDTH * 3; ++i) {
+	bytes[i] = DEAD;
+}
 main();
 function main() {
-	generate_seed(ctx);
-	conway();
+	generate_seed();
+	requestAnimationFrame(conway);
 }
-function generate_seed(ctx: CanvasRenderingContext2D) {
+function generate_seed() {
 	let number_of_alive = Math.ceil(Math.random() * (BUFFER_HEIGHT * BUFFER_WIDTH));
 	for (let i = 0; i < number_of_alive; ++i) {
 		let col = Math.floor(Math.random() * BUFFER_WIDTH);
 		let row = Math.floor(Math.random() * BUFFER_HEIGHT);
-		bytes[(row * BUFFER_WIDTH * 3) + (col * 3)] = 1;
-		bytes[(row * BUFFER_WIDTH * 3) + (col * 3) + 1] = 1;
-		bytes[(row * BUFFER_WIDTH * 3) + (col * 3) + 2] = 1;
+		let curr_first = bytes[(row * BUFFER_WIDTH * 3) + (col * 3)] = ALIVE;
+		let curr_second = bytes[(row * BUFFER_WIDTH * 3) + (col * 3) + 1] = ALIVE;
+		let curr_third = bytes[(row * BUFFER_WIDTH * 3) + (col * 3) + 2] = ALIVE;
 		ctx.beginPath();
+		ctx.fillStyle = `rgb(${curr_first * 255} ${curr_second * 255} ${curr_third * 255})`;
 		ctx.rect(col * NODE_WIDTH, row * NODE_HEIGHT, NODE_WIDTH, NODE_HEIGHT);
 		ctx.fill();
 	}
@@ -33,7 +39,7 @@ function apply_conway_rules() {
 				let first = bytes[((r+1) * BUFFER_WIDTH * 3) + (c*3)];
 				let second = bytes[((r+1) * BUFFER_WIDTH * 3) + (c*3) + 1];
 				let third = bytes[((r+1) * BUFFER_WIDTH * 3) + (c*3) + 2];
-				if (first > 0 || second > 0 || third > 0) {
+				if (first == ALIVE || second == ALIVE || third == ALIVE) {
 					live_neighbors += 1;
 				}
 			}
@@ -41,7 +47,7 @@ function apply_conway_rules() {
 				let first = bytes[((r) * BUFFER_WIDTH * 3) + ((c+1)*3)];
 				let second = bytes[((r) * BUFFER_WIDTH * 3) + ((c+1)*3) + 1];
 				let third = bytes[((r) * BUFFER_WIDTH * 3) + ((c+1)*3) + 2];
-				if (first > 0 || second > 0 || third > 0) {
+				if (first == ALIVE || second == ALIVE || third == ALIVE) {
 					live_neighbors += 1;
 				}
 			}
@@ -49,7 +55,7 @@ function apply_conway_rules() {
 				let first = bytes[((r+1) * BUFFER_WIDTH * 3) + ((c+1)*3)];
 				let second = bytes[((r+1) * BUFFER_WIDTH * 3) + ((c+1)*3) + 1];
 				let third = bytes[((r+1) * BUFFER_WIDTH * 3) + ((c+1)*3) + 2];
-				if (first > 0 || second > 0 || third > 0) {
+				if (first == ALIVE || second == ALIVE || third == ALIVE) {
 					live_neighbors += 1;
 				}
 			}
@@ -57,7 +63,7 @@ function apply_conway_rules() {
 				let first = bytes[((r-1) * BUFFER_WIDTH * 3) + ((c)*3)];
 				let second = bytes[((r-1) * BUFFER_WIDTH * 3) + ((c)*3) + 1];
 				let third = bytes[((r-1) * BUFFER_WIDTH * 3) + ((c)*3) + 2];
-				if (first > 0 || second > 0 || third > 0) {
+				if (first == ALIVE || second == ALIVE || third == ALIVE) {
 					live_neighbors += 1;
 				}
 			}
@@ -65,7 +71,7 @@ function apply_conway_rules() {
 				let first = bytes[((r) * BUFFER_WIDTH * 3) + ((c - 1)*3)];
 				let second = bytes[((r) * BUFFER_WIDTH * 3) + ((c - 1)*3) + 1];
 				let third = bytes[((r) * BUFFER_WIDTH * 3) + ((c - 1)*3) + 2];
-				if (first > 0 || second > 0 || third > 0) {
+				if (first == ALIVE || second == ALIVE || third == ALIVE) {
 					live_neighbors += 1;
 				}
 			}
@@ -73,7 +79,7 @@ function apply_conway_rules() {
 				let first = bytes[((r - 1) * BUFFER_WIDTH * 3) + ((c - 1)*3)];
 				let second = bytes[((r - 1) * BUFFER_WIDTH * 3) + ((c - 1)*3) + 1];
 				let third = bytes[((r - 1) * BUFFER_WIDTH * 3) + ((c - 1)*3) + 2];
-				if (first > 0 || second > 0 || third > 0) {
+				if (first == ALIVE || second == ALIVE || third == ALIVE) {
 					live_neighbors += 1;
 				}
 			}
@@ -81,7 +87,7 @@ function apply_conway_rules() {
 				let first = bytes[((r + 1) * BUFFER_WIDTH * 3) + ((c - 1)*3)];
 				let second = bytes[((r + 1) * BUFFER_WIDTH * 3) + ((c - 1)*3) + 1];
 				let third = bytes[((r + 1) * BUFFER_WIDTH * 3) + ((c - 1)*3) + 2];
-				if (first > 0 || second > 0 || third > 0) {
+				if (first == ALIVE || second == ALIVE || third == ALIVE) {
 					live_neighbors += 1;
 				}
 			}
@@ -89,40 +95,35 @@ function apply_conway_rules() {
 				let first = bytes[((r - 1) * BUFFER_WIDTH * 3) + ((c + 1)*3)];
 				let second = bytes[((r - 1) * BUFFER_WIDTH * 3) + ((c + 1)*3) + 1];
 				let third = bytes[((r - 1) * BUFFER_WIDTH * 3) + ((c + 1)*3) + 2];
-				if (first > 0 || second > 0 || third > 0) {
+				if (first == ALIVE || second == ALIVE || third == ALIVE) {
 					live_neighbors += 1;
 				}
 			}
 			if (live_neighbors < 2 || live_neighbors > 3) {
-				bytes[(r * BUFFER_WIDTH * 3) + (c*3)] = 0;
-				bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 1] = 0;
-				bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 2] = 0;
+				bytes[(r * BUFFER_WIDTH * 3) + (c*3)] = DEAD;
+				bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 1] = DEAD;
+				bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 2] = DEAD;
 			} else if (live_neighbors == 3) {
-				bytes[(r * BUFFER_WIDTH * 3) + (c*3)] = 1;
-				bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 1] = 1;
-				bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 2] = 1;
+				bytes[(r * BUFFER_WIDTH * 3) + (c*3)] = ALIVE;
+				bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 1] = ALIVE;
+				bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 2] = ALIVE;
 			}
 		}
 	}
 }
 function draw_bytes() {
-	//ctx.clearRect(0, 0, cvs.width, cvs.height);
-	ctx.beginPath();
+	ctx.clearRect(0, 0, cvs.width, cvs.height);
 	for (let r = 0; r < BUFFER_HEIGHT; ++r) {
 		for (let c = 0; c < BUFFER_WIDTH; ++c) {
 			let curr_first = bytes[(r * BUFFER_WIDTH * 3) + (c*3)];
 			let curr_second = bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 1];
 			let curr_third = bytes[(r * BUFFER_WIDTH * 3) + (c*3) + 2];
-			if (curr_first > 0 || curr_second > 0 || curr_third > 0) {
-				ctx.fillStyle = `rgb(${curr_first}, ${curr_second}, ${curr_third}, 0)`;
-				ctx.rect(c * NODE_WIDTH, r * NODE_HEIGHT, NODE_WIDTH, NODE_HEIGHT);
-			} else {
-				//ctx.fillStyle = `rgb(0, 0, 0, 0)`;
-				//ctx.rect(c * NODE_WIDTH, r * NODE_HEIGHT, NODE_WIDTH, NODE_HEIGHT);
-			}
+			ctx.beginPath();
+			ctx.fillStyle = `rgb(${curr_first * 255} ${curr_second * 255} ${curr_third * 255})`;
+			ctx.rect(c * NODE_WIDTH, r * NODE_HEIGHT, NODE_WIDTH, NODE_HEIGHT);
+			ctx.fill();
 		}
 	}
-	ctx.fill();
 }
 function conway() {
 	apply_conway_rules();
